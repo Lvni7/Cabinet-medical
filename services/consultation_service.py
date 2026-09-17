22e91af2b89465a0fbb1d1dd2e46d3e627866ef7
gabriel C <gabcab1002@gmail.com>
2026-09-17 16:31:12 +0200

    feat: rajout de methode dans consultations service

--- services/consultation_service.py
+++ services/consultation_service.py
@@ -6,8 +6,9 @@ class ConsultationService:
     def add_consultation(self, consultation):
         self.__consultations.append(consultation)
 
-    def get_consultations(self):
-        return self.__consultations
+    def get_consultations(self, patient):
+        return [consultation for consultation in self.__consultations if consultation.patient == patient]
+
     def create_consultation(appointment_datetime, patient, doctor, diagnosis, prescriptions, status):
         """
         Planifie une consultation en créant une instance de la classe Consultation.
@@ -24,3 +25,26 @@ class ConsultationService:
             Consultation: Une instance de la classe Consultation représentant la consultation planifiée.
         """
         return Consultation(appointment_datetime, patient, doctor, diagnosis, prescriptions, status)
+
+    def add_consultation_to_patient(self, patient, consultation):
+        """
+        Ajoute une consultation à la liste des consultations d'un patient.
+
+        Args:
+            patient (Patient): L'objet représentant le patient.
+            consultation (Consultation): L'objet représentant la consultation à ajouter.
+        """
+        patient.add_consultation(consultation)
+
+    def get_consultations_by_patient(self, patient):
+        """
+        Récupère toutes les consultations d'un patient donné.
+
+        Args:
+            patient (Patient): L'objet représentant le patient.
+
+        Returns:
+            list: Une liste contenant toutes les consultations du patient.
+        """
+        return self.get_consultations(patient)
+    
